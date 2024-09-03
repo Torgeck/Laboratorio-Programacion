d22e23869c6f06ce53e6cd9198d7f583a3a55a8b
Torgeck <54917534+Torgeck@users.noreply.github>
2024-09-03 17:46:06 -0300

    Cambio de atributos publicos por privados, correcciones de variables sin this keyword

--- TP-Concurrencia_Patrones/handler/PixelHandler.java
+++ TP-Concurrencia_Patrones/handler/PixelHandler.java
@@ -2,10 +2,10 @@ package handler;
 
 import objects.SheetPrint;
 
-public class PixelHandler extends Handler{
-    public String tipo;
-    public String emoji;
-    public String nombreTipo;
+public class PixelHandler extends Handler {
+    private String tipo;
+    private String emoji;
+    private String nombreTipo;
 
     public PixelHandler(Handler succ, String unTipo, String unEmoji, String unNombreTipo) {
         super(succ);
@@ -19,8 +19,8 @@ public class PixelHandler extends Handler{
 
         if (hoja.getPixel(i, j).equals(this.tipo)) {
             // setea el pixel en el color que corresponda segun el tipo.
-            hoja.setPixel(emoji, i, j);
-            System.out.printf("Se imprimio el pixel de %s en %s\n",hoja.getName(),nombreTipo);
+            hoja.setPixel(this.emoji, i, j);
+            System.out.printf("Se imprimio el pixel de %s en %s\n", hoja.getName(), this.nombreTipo);
             exito = true;
         } else {
             exito = this.getSuccesor().handleRequest(hoja, i, j);
