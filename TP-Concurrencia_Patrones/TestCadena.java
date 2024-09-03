103f87fca41a888293c896945017990fc2fddceb
GonzaHCB <gonzalo.cabanne@est.fi.uncoma.edu.ar>
2024-09-02 23:55:56 -0300

    Cambio algo que estaba mal en el main (pruebas pasadas)

--- TP-Concurrencia_Patrones/TestCadena.java
+++ TP-Concurrencia_Patrones/TestCadena.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.Executors;
 
 import handler.*;
 import objects.Client;
@@ -9,13 +9,13 @@ public class TestCadena {
 
         public static void main(String[] args) {
                 System.out.println("Inicio del programa");
-                ScheduledExecutorService exc = Executors.newSingleThreadScheduledExecutor();
+                ScheduledExecutorService executor = new ScheduledThreadPoolExecutor(1);
                 WhiteHandler white = new WhiteHandler(null);
                 BlackHandler black = new BlackHandler(white);
                 ColorHandler color = new ColorHandler(black);
                 HoloHandler holo = new HoloHandler(color);
 
-                exc.scheduleAtFixedRate(new Client("Alberto", holo, 3, 3), 3, 15, TimeUnit.SECONDS);
+                executor.scheduleAtFixedRate(new Client("Alberto", holo, 3, 3), 3, 15, TimeUnit.SECONDS);
 
                 //exc.shutdown();
         }
