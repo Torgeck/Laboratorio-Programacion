a3e8bf919f4eccef356cce922a3ececf71698942
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-02 20:13:15 -0300

    Modificado script.js

--- TP-Web_estatica/script.js
+++ TP-Web_estatica/script.js
@@ -1,57 +1,7 @@
-export function generarHeader() {
-  const header = document.querySelector("header");
-  const logo = document.createElement("img");
-  const boton = document.createElement("button");
-  const seccionCervezas = document.createElement("a");
-  const seccionCortes = document.createElement("a");
-  const cajaButonera = document.createElement("div");
+import { generarHeader, generarFooter } from "./utils.js";
 
-  logo.setAttribute("src", "./assets/imgs/logoCompleto.png");
-  logo.setAttribute("class", "logo");
-  logo.setAttribute("alt", "Logo de la barberia");
-
-  seccionCervezas.textContent = "Nuestras Cervezas";
-  seccionCervezas.setAttribute("href", "./pages/beers/beers.html");
-
-  seccionCortes.textContent = "Galeria Cortes";
-  seccionCortes.setAttribute("href", "./pages/haircuts/haircuts.html");
-
-  cajaButonera.setAttribute("class", "botonera");
-
-  boton.textContent = "Reserva tu turno";
-  //boton.addEventListener("click", pedirTurno);
-
-  logo.addEventListener("click", () => (location.href = "./index.html"));
-
-  cajaButonera.append(seccionCervezas, seccionCortes);
-  header.append(logo, cajaButonera, boton);
-}
-export function generarFooter() {
-  const footer = document.querySelector("footer");
-  const linkInsta = document.createElement("a");
-  const linkTik = document.createElement("a");
-  const insta = document.createElement("img");
-  const tik = document.createElement("img");
-
-  insta.setAttribute("src", "./assets/icons/instagram.svg");
-  insta.setAttribute("class", "socials");
-  insta.setAttribute("alt", "Icono Instagram");
-  linkInsta.setAttribute("href", "https://www.instagram.com/labar.beer/");
-  linkInsta.setAttribute("target", "_blank");
-  linkInsta.setAttribute("rel", "noopener noreferrer");
-
-  tik.setAttribute("src", "./assets/icons/tiktok.svg");
-  tik.setAttribute("class", "socials");
-  tik.setAttribute("alt", "Icono TikTok");
-  linkTik.setAttribute("href", "https://www.tiktok.com/@labar.beer");
-  linkTik.setAttribute("target", "_blank");
-  linkTik.setAttribute("rel", "noopener noreferrer");
-
-  linkInsta.appendChild(insta);
-  linkTik.appendChild(tik);
-  footer.append(linkInsta, linkTik);
-}
-
+generarHeader("");
+generarFooter("");
 /*
 //Simular turnos cada 30 minutos en horario laboral para la semana anterior y la siguiente
 const ahora = new Date();
@@ -83,6 +33,7 @@ for(let i=0;i<14;i++){
   */
 //Colocarlos en algún HTML...
 
+/*
 let elem; //Variable auxiliar para crear elementos
 //Insertar cortes
 let cantCortes = 6;
@@ -105,6 +56,4 @@ for (let i = 1; i <= cantCervezas; i++) {
   //elem.style.width = "500px";
   cervezas.appendChild(elem);
 }
-
-generarFooter();
-generarHeader();
+  */
