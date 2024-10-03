7d1b2cf6546d39234d1ffa1d6a24b39d033cf13b
Torgeck <54917534+Torgeck@users.noreply.github.com>
2024-10-03 00:20:37 -0300

    Agregado logica basica en la pagina de cervezas

--- TP-Web_estatica/pages/beers/beers.js
+++ TP-Web_estatica/pages/beers/beers.js
@@ -1,5 +1,37 @@
 import { generarHeader, generarFooter, generarCervezas } from "../../utils.js";
+const mapCervezas = new Map([
+  [
+    "Cruzando el puente",
+    "Chispeante, alegre, cítrica y refrescante. De notas a maracuyá, pomelo y ananá ",
+  ],
+  [
+    "Dolce Vita",
+    "Cerveza rubia, aromática, ligera, seca y muy refrescante. Presencia de lúpulo floral - frutado. ibu 31 srm 3.5 abv 4,5%",
+  ],
+  [
+    "Brava",
+    "Cerveza roja. seca, fuerte y lupulada. Soporte de maltas de perfil tostado, bizcocho y caramelo. ibu 62 srm 12 abv 7%",
+  ],
+  [
+    "Deporter",
+    "Cerveza negra. fuerte y de carácter maltoso. Notas a caramelo, chocolate y toffee. ibu 32 srm 42 abv 6%",
+  ],
+  ["IPA Moderna", "Es como una IPA pero esta viene adicta al skibidi toilet"],
+  [
+    "Del Limay",
+    "Cerveza rubia. equilibrada, orientada a la malta con un suave lupulado floral. abv %5 srm 4 ibu 18",
+  ],
+  [
+    "Sueño Venice",
+    "Cerveza rubia. equilibrada, orientada a la malta con un suave lupulado floral. abv %5 srm 4 ibu 18",
+  ],
+]);
+let i = 1;
 
 generarHeader("../../");
 generarFooter("../../");
-generarCervezas();
+generarCervezas(mapCervezas);
+
+function agregarCerveza(nombre, descripcion) {
+  mapCervezas.set(nombre, descripcion);
+}
