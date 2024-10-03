ea43d42b57b9c8ea98284b3cd1df3d7a6a7bbab2
GonzaHCB <gonzalo.cabanne@est.fi.uncoma.edu.ar>
2024-10-03 19:47:19 -0300

    se corrige links de header y footer, e imagenes no cargando

--- TP-Web_estatica/script.js
+++ TP-Web_estatica/script.js
@@ -1,14 +1,6 @@
 
-const direLogo = 'assets/imgs/logoCompleto.png';
-const direPagCervezas = 'pages/beers/beers.html';
-const direPagCortes = 'pages/haircuts/haircuts.html';
-const direPagPrincipal = 'index.html';
-
-const direIconoTikTok = 'assets/icons/tiktok.svg'
-const direIconoInsta = 'assets/icons/instagram.svg';
-
-generarFooter(direIconoTikTok,direIconoInsta);
-generarHeader(direLogo,direPagCervezas,direPagCortes,direPagPrincipal);
+generarFooter("");
+generarHeader("");
 /*
 //Simular turnos cada 30 minutos en horario laboral para la semana anterior y la siguiente
 const ahora = new Date();
