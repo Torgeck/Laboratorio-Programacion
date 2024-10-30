0681e654351f3c9317d3eccbe71cff1dd4227273
Torgeck <54917534+Torgeck@users.noreply.github>
2024-10-30 17:09:59 -0300

    Agregado a .gitignore la carpeta de node

--- TP-Web_dinamica/server/api.js
+++ TP-Web_dinamica/server/api.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app=express();
-const port=9000;
+const express = require("express");
+const app = express();
+const port = 9000;
+const path = require("path");
 
-app.use(express.static("./../static/"));
+app.use("/", express.static(path.join(__dirname, "../static")));
 
-app.get('/', (req, res) => {
-  //app.use(express.static("./../static/index.html"));
-  res.sendFile("index.html");
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile("404.html");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
