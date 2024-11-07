44e65f01bf198e2f20bbba9dff25ae41102eb30c
Torgeck <54917534+Torgeck@users.noreply.github>
2024-11-07 19:49:03 -0300

    Actualizado codigo

--- TP-Web_dinamica/server/controllers/beersController.js
+++ TP-Web_dinamica/server/controllers/beersController.js
@@ -1,9 +1,4 @@
-const data = {
-  beers: require("../data/beers.json"),
-  setBeers: function (data) {
-    this.beers = data;
-  },
-};
+const modelBeer = require("../model/modelBeer");
 
 const notFound = (res, msg) => {
   res.status(400).json({ message: msg });
@@ -14,69 +9,54 @@ const getAllBeers = (req, res) => {
 };
 
 const createNewBeer = (req, res) => {
-  const newBeer = {
-    id: data.beers[data.beers.length - 1].id + 1 || 1,
-    name: req.body.name,
-    description: req.body.description,
-  };
+  const name = req.body.name;
+  const desc = req.body.description;
 
-  if (!newBeer.name || !newBeer.description) {
+  if (!name || !desc) {
     notFound(res, "Nombre y descripcion no pueden ser vacios");
   } else {
-    data.setBeers = [...data.beers, newBeer];
-    res.status(201).json(data.beers);
+    modelBeer.createNewBeer(name, desc);
+    res.status(201);
   }
 };
 
 const updateBeer = (req, res) => {
-  const beer = data.beers.find((elem) => elem.id === parseInt(req.body.id));
+  const id = req.body.id;
+  const beer = modelBeer.findBeer(req.body.id);
   const beerName = req.body.name;
   const beerDesc = req.body.description;
 
   if (!beer) {
-    notFound(res, `Cerveza con ID ${req.body.id} no encontrada`);
+    notFound(res, `Cerveza con ID ${id} no encontrada`);
   } else {
     if (beerName) beer.name = beerName;
     if (beerDesc) beer.description = beerDesc;
 
-    // "Saco" del arreglo a la cerveza actualizada para luego agregarla
-    const filteredArray = data.beers.filter(
-      (elem) => elem.id !== parseInt(req.body.id)
-    );
-    const unsortedArray = [...filteredArray, beer];
-
-    // Ordeno el arreglo por id
-    data.setBeers(
-      unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
-    );
-    res.status(201).json(data.beers);
+    modelBeer.updateBeer(id);
+    res.status(201);
   }
 };
 
 const deleteBeer = (req, res) => {
-  const beer = data.beer.find((elem) => elem.id === parseInt(req.body.id));
+  const id = req.body.id;
+  const beer = modelBeer.findBeer(id);
 
   if (!beer) {
-    notFound(res, `Cerveza con ID ${req.body.id} no encontrada`);
+    notFound(res, `Cerveza con ID ${id} no encontrada`);
   } else {
-    // Elimino a la cerveza si la encuentro
-    const filteredArray = data.beers.filter(
-      (elem) => elem.id === parseInt(req.body.id)
-    );
-
-    data.setBeers([...filteredArray]);
-    // Devuelvo todo el arreglo de cervezas, podria ser cualquier otra cosa
-    res.status(201).json(data.beers);
+    modelBeer.deleteBeer(id);
+    res.status(201);
   }
 };
 
 const getBeer = (req, res) => {
-  const beer = data.beers.find((elem) => elem.id === req.body.id);
+  const id = req.body.id;
+  const beer = modelBeer.findBeer(id);
 
   if (!beer) {
-    notFound(res, `Cerveza con ID ${req.body.id} no encontrada`);
+    notFound(res, `Cerveza con ID ${id} no encontrada`);
   } else {
-    res.status(201).json(beer);
+    res.status(201);
   }
 };
 
---
+++ TP-Web_dinamica/server/model/modelBeer.js
@@ -0,0 +1,44 @@
+const data = {
+  beers: require("../data/beers.json"),
+  setBeers: function (data) {
+    this.beers = data;
+  },
+};
+
+const createBeer = (name, description) => {
+  const newBeer = {
+    id: data.beers[data.beers.length - 1].id + 1 || 1,
+    name: name,
+    description: description,
+  };
+
+  data.setBeers = [...data.beers, newBeer];
+  return true;
+};
+
+const findBeer = (id) => {
+  return data.beers.find((elem) => elem.id === parseInt(id));
+};
+
+const updateBeer = (id) => {
+  const filteredArray = data.beers.filter((elem) => elem.id !== parseInt(id));
+  const unsortedArray = [...filteredArray, beer];
+
+  data.setBeers(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+  return true;
+};
+
+const deleteBeer = (id) => {
+  let resp = false;
+  if (!data.beers.find(id)) {
+    const filteredArray = data.beers.filter(
+      (elem) => elem.id !== parseInt(req.body.id)
+    );
+    data.setBeers([...filteredArray]);
+    resp = true;
+  }
+
+  return resp;
+};
